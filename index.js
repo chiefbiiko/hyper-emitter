a319338b524622747eba0a0ebefa32ba950d65df
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-06-16 21:56:49 +0200

    works with plain objects

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 var EventEmitter = require('events').EventEmitter
+var dealias = require('aka-opts')
 
-function hyperEmitter (obj, opts) {
+function shouldEmit (ok, only, key) {
+  return ok && !only.length || ok && only.length && only.indexOf(key) !== -1
+}
+
+function hyperEmitter (target, opts) {
+  opts = Object.assign({ only: [] }, dealias(opts || {}, { only: [ 'keys' ] }))
   var setup = false
   var handler = {
     defineProperty (target, key, descriptor) {
       var ok = Reflect.defineProperty(target, key, descriptor)
-      if (ok) proxy.emit('defineProperty', target, key, descriptor)
+      if (shouldEmit(ok, opts.only, key)) {
+        proxy.emit('didDefineProperty', target, key, descriptor)
+      }
       return ok
     },
-    deleteProperty (target, property) {
-      var ok = Reflect.deleteProperty(target, property)
-      if (ok) proxy.emit('deleteProperty', target, property)
+    deleteProperty (target, key) {
+      var ok = Reflect.deleteProperty(target, key)
+      if (shouldEmit(ok, opts.only, key)) {
+        proxy.emit('didDeleteProperty', target, key)
+      }
       return ok
     },
-    set (target, property, value, receiver) {
-      var ok = Reflect.set(target, property, value, receiver)
-      if (ok) proxy.emit('set', target, property, value, receiver)
+    set (target, key, value, receiver) {
+      var ok = Reflect.set(target, key, value, receiver)
+      if (shouldEmit(ok, opts.only, key)) {
+        proxy.emit('didSet', target, key, value, receiver)
+      }
       return ok
     },
     setPrototypeOf (target, prototype) {
       var ok = Reflect.setPrototypeOf(target, prototype)
       if (!setup) setup = true
-      if (ok && setup) proxy.emit('setPrototypeOf', target, prototype)
+      if (ok && setup) proxy.emit('didSetPrototypeOf', target, prototype)
       return ok
     }
   }
-  var proxy = new Proxy(obj, Object.assign(handler, opts || {}))
+  var proxy = new Proxy(target, Object.assign(handler, opts))
   Object.setPrototypeOf(proxy, EventEmitter.prototype)
   return proxy
 }
--- test.js
+++ test.js
@@ -4,8 +4,52 @@ var EventEmitter = require('events').EventEmitter
 
 tape('hyperEmitter mixes in EventEmitter.prototype', function (t) {
   var hyperObject = hyperEmitter({ a: 1, b: 419 })
-  t.ok(Object.keys(EventEmitter.prototype).every(function (property) {
-    return property in hyperObject
+  t.ok(Object.keys(EventEmitter.prototype).every(function (key) {
+    return key in hyperObject
   }), 'hyperObject got all EventEmitter prototype methods mixed in')
   t.end()
+})
+
+tape('emits on (after) simple property mutation', function (t) {
+  var hyperObject = hyperEmitter({ a: 1, b: 419 })
+  hyperObject.on('didSet', function (target, property, value, receiver) {
+    if (property.startsWith('_')) return
+    t.is(hyperObject[property], 187, 'hyperObject.' + property + ' hyper set')
+    t.end()
+  })
+  hyperObject.a = 187
+})
+
+tape('emits on (after) property definition', function (t) {
+  var hyperObject = hyperEmitter({ a: 1, b: 419 })
+  hyperObject.on('didDefineProperty', function (target, key, descriptor) {
+    if (key.startsWith('_')) return
+    t.is(hyperObject[key], descriptor.value, 'defined correctly')
+    t.end()
+  })
+  hyperObject.a = 187
+})
+
+tape('emits on (after) property deletion', function (t) {
+  var hyperObject = hyperEmitter({ a: 1, b: 419 }, { only: [ 'a' ] })
+  hyperObject.on('didDeleteProperty', function (target, key) {
+    t.notOk(hyperObject[key], 'deleted')
+    t.end()
+  })
+  delete hyperObject.a
+})
+
+tape('emits on (after) prototype mutation', function (t) {
+  var hyperObject = hyperEmitter({ a: 1, b: 419 })
+  hyperObject.on('didSetPrototypeOf', function (target, prototype) {
+    t.same(Object.getPrototypeOf(hyperObject), prototype, 'same')
+    t.end()
+  })
+  var prototype =
+    Object.assign({ noop: function () {} }, Object.getPrototypeOf(hyperObject))
+  Object.setPrototypeOf(hyperObject, prototype)
+})
+
+tape('some exiting type', function (t) {
+  t.false(true)
 })
